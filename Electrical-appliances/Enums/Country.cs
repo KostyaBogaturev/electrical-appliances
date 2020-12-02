c7a0f51f3f9c8b8c0309dd1415550d8cf1878b94
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-03 01:36:17 +0200

    Added new folder Enums and new enum Country

---
+++ Electrical-appliances/Enums/Country.cs
@@ -0,0 +1,28 @@
+﻿namespace Electrical_appliances.Enums
+{
+    /// <summary>
+    /// This enum fo countries.
+    /// </summary>
+    public enum Country
+    {
+        /// <summary>
+        /// USA.
+        /// </summary>
+        USA,
+
+        /// <summary>
+        /// France.
+        /// </summary>
+        France,
+
+        /// <summary>
+        /// Japan.
+        /// </summary>
+        Japan,
+
+        /// <summary>
+        /// China.
+        /// </summary>
+        China,
+    }
+}
