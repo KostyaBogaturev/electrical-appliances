dd03fef75764432c555f0573f3e98d4403b3329d
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-03 01:18:37 +0200

    Added new folder "Abstractions" and new interface "IConsumerEnergy"

---
+++ Electrical-appliances/Abstractions/IConsumerEnergy.cs
@@ -0,0 +1,28 @@
+﻿namespace Electrical_appliances.Abstractions
+{
+    /// <summary>
+    /// Inteface ,which discribe thing which consume energy.
+    /// </summary>
+    public interface IConsumerEnergy
+    {
+        /// <summary>
+        /// Gets for height.
+        /// </summary>
+        public double Height { get; }
+
+        /// <summary>
+        /// Gets for length.
+        /// </summary>
+        public double Length { get;  }
+
+        /// <summary>
+        /// Gets for width.
+        /// </summary>
+        public double Width { get; }
+
+        /// <summary>
+        /// Gets or sets for Power Consumption.
+        /// </summary>
+        public int PowerConsumption { get; set; }
+    }
+}
