8d0ee9843d18df2e1c5355600ae86c8c1b73eea9
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-03 00:24:58 +0200

    Created a new solution

---
+++ Electrical-appliances/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Electrical_appliances
+{
+    using System;
+
+    /// <summary>
+    /// Start method for programm.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// start point for programm.
+        /// </summary>
+        /// <param name="args">args parametr.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
