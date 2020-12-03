f07a55816a595c6a5eed1c6fa8fa94aa8ff655be
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-03 20:19:08 +0200

    Added class Fridge

---
+++ Electrical-appliances/Models/Technics/Fridge.cs
@@ -0,0 +1,38 @@
+﻿namespace Electrical_appliances.Models.Technics
+{
+    using System;
+    using Electrical_appliances.Abstractions;
+    using Electrical_appliances.Enums;
+
+    /// <summary>
+    /// Class Washing Machine.
+    /// </summary>
+    public class Fridge : Appliances
+    {
+        private bool isTurnOn;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Fridge"/> class.
+        /// </summary>
+        /// <param name="price">price.</param>
+        /// <param name="productionPlace"> Place of production.</param>
+        public Fridge(int price, Country productionPlace)
+        : base(2.3, price, productionPlace, 420, Devices.Fridge)
+        {
+            this.isTurnOn = false;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether is device turn on.
+        /// </summary>
+        public override bool IsTurnOn { get => this.isTurnOn; set => this.isTurnOn = value; }
+
+        /// <summary>
+        /// Use kettle.
+        /// </summary>
+        public override void Use()
+        {
+            Console.WriteLine("I washed a clothes");
+        }
+    }
+}
