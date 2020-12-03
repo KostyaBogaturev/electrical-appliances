c55d6af6c5c3bafb8f80a5ce2cf8e2eea1517eea
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-03 16:32:04 +0200

    Added new abstract class Appliances

---
+++ Electrical-appliances/Abstractions/Appliances.cs
@@ -0,0 +1,74 @@
+﻿namespace Electrical_appliances.Abstractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Electrical_appliances.Enums;
+
+    /// <summary>
+    /// Abstract class witch describe electro. devices.
+    /// </summary>
+    public abstract class Appliances : IConsumerEnergy
+    {
+        private readonly double volume;
+        private readonly int price;
+        private readonly Country productionPlace;
+        private readonly int powerConsumption;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Appliances"/> class.
+        /// </summary>
+        /// <param name="volume">Volume of appliances.</param>
+        /// <param name="price">Price of appliances.</param>
+        /// <param name="productionPlace">Place of prodaction of appliances.</param>
+        /// <param name="powerConsumption">Consumption of power of appliances.</param>
+        public Appliances(double volume, int price, Country productionPlace, int powerConsumption)
+        {
+            this.volume = volume;
+            this.price = price;
+            this.productionPlace = productionPlace;
+            this.powerConsumption = powerConsumption;
+        }
+
+        /// <summary>
+        /// Gets implement interface property volume.
+        /// </summary>
+        public double Volume => this.volume;
+
+        /// <summary>
+        /// Gets implement interface property price.
+        /// </summary>
+        public int Price => this.price;
+
+        /// <summary>
+        /// Gets or sets implement interface property volume.
+        /// </summary>
+        public int PowerConsumption
+        {
+            get
+            {
+                return this.powerConsumption;
+            }
+
+            set
+            {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Gets implement interface property place of production.
+        /// </summary>
+        public Country ProductionPlace => this.productionPlace;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether is this appliances turn on.
+        /// </summary>
+        public abstract bool IsTurnOn { get; set; }
+
+        /// <summary>
+        /// This method return what make appliances which we use.
+        /// </summary>
+        protected abstract void Use();
+    }
+}
