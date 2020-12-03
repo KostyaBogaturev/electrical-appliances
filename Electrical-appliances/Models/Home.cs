26933d84225308fb96202e42af02df0ea26fe1db
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-03 02:34:59 +0200

    Added new folder models and new class Home

---
+++ Electrical-appliances/Models/Home.cs
@@ -0,0 +1,84 @@
+﻿namespace Electrical_appliances.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Electrical_appliances.Abstractions;
+    using Electrical_appliances.Enums;
+
+    /// <summary>
+    /// Describe class home.
+    /// </summary>
+    public class Home : IConsumerEnergy
+    {
+        private readonly double volume;
+        private readonly int price;
+        private readonly Country productionPlace;
+
+        private int powerConsumption;
+        private int[] socets;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Home"/> class.
+        /// </summary>
+        /// <param name="volume">volume of house.</param>
+        /// <param name="price">price of house.</param>
+        /// <param name="productionPlace">place of production of house.</param>
+        /// <param name="countOfSocets">count of socets at house.</param>
+        public Home(double volume, int price, Country productionPlace, int countOfSocets)
+        {
+            this.volume = volume;
+            this.price = price;
+            this.productionPlace = productionPlace;
+            this.powerConsumption = 0;
+            this.socets = new int[countOfSocets];
+        }
+
+        /// <summary>
+        /// Gets or sets ASDASD.
+        /// </summary>
+        public int[] Socets
+        {
+            get
+            {
+                return this.socets;
+            }
+
+            set
+            {
+                this.socets = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets implement interface property volume.
+        /// </summary>
+        public double Volume => this.volume;
+
+        /// <summary>
+        /// Gets implement interface property price.
+        /// </summary>
+        public int Price => this.price;
+
+        /// <summary>
+        /// Gets or sets implement interface property volume.
+        /// </summary>
+        public int PowerConsumption
+        {
+            get
+            {
+                return this.powerConsumption;
+            }
+
+            set
+            {
+                this.powerConsumption = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets implement interface property place of production.
+        /// </summary>
+        public Country ProductionPlace => this.productionPlace;
+    }
+}
